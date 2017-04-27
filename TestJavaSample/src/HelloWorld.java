310ed5839d019c9eb15edd38b46df3b77eadb0a6
bobbyjohn.murray <john.murray@callhealth.co.in>
2017-04-27 14:53:56 +0530

    create new program

---
+++ TestJavaSample/src/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author bobbyjohn.murray
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(" hello how r u");
+
+	}
+
+}
